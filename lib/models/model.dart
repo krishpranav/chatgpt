91ac00553ca27b3e2120c20490af7f38b65461fe
krishpranav <krisna.pranav@gmail.com>
2023-04-22 12:16:51 +0530

    models: chat_models[specified chat models], model[base model for the app]

---
+++ lib/models/model.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class ModelsModel {
+  final String id;
+  final int created;
+  final String root;
+
+  ModelsModel({
+    required this.id,
+    required this.root,
+    required this.created,
+  });
+
+  factory ModelsModel.fromJson(Map<String, dynamic> json) => ModelsModel(id: json["id"], root: json["root"], created: json["created"])
+}
