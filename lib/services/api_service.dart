7d19f7d99047df137b48bd396c8d7a1b33d0b801
krishpranav <krisna.pranav@gmail.com>
2023-04-22 13:06:44 +0530

    services: api_services + assets_service
    api_service[gets & calls api services], assets_services[deals with assets]

---
+++ lib/services/api_service.dart
@@ -0,0 +1,115 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:chatgpt/models/chat_models.dart';
+import 'package:chatgpt/api/api_const.dart';
+import 'package:chatgpt/models/model.dart';
+
+class ApiService {
+  static Future<List<ModelsModel>> getModels() async {
+    try {
+      var response = await http.get(
+        Uri.parse("$BASE_URL/models"),
+        headers: {'Authorization': 'Bearer $API_KEY'},
+      );
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      List temp = [];
+      for (var value in jsonResponse["data"]) {
+        temp.add(value);
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  static Future<List<ChatModel>> sendMessageGPT(
+      {required String message, required String modelId}) async {
+    try {
+      var response = await http.post(
+        Uri.parse("$BASE_URL/chat/completions"),
+        headers: {
+          'Authorization': 'Bearer $API_KEY',
+          "Content-Type": "application/json"
+        },
+        body: jsonEncode(
+          {
+            "model": modelId,
+            "messages": [
+              {
+                "role": "user",
+                "content": message,
+              }
+            ]
+          },
+        ),
+      );
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse["choices"].length > 0) {
+        chatList = List.generate(
+          jsonResponse["choices"].length,
+          (index) => ChatModel(
+            msg: jsonResponse["choices"][index]["message"]["content"],
+            chatIndex: 1,
+          ),
+        );
+      }
+      return chatList;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  static Future<List<ChatModel>> sendMessage(
+      {required String message, required String modelId}) async {
+    try {
+      var response = await http.post(
+        Uri.parse("$BASE_URL/completions"),
+        headers: {
+          'Authorization': 'Bearer $API_KEY',
+          "Content-Type": "application/json"
+        },
+        body: jsonEncode(
+          {
+            "model": modelId,
+            "prompt": message,
+            "max_tokens": 300,
+          },
+        ),
+      );
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse["choices"].length > 0) {
+        chatList = List.generate(
+          jsonResponse["choices"].length,
+          (index) => ChatModel(
+            msg: jsonResponse["choices"][index]["text"],
+            chatIndex: 1,
+          ),
+        );
+      }
+      return chatList;
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
---
+++ lib/services/assets_service.dart
@@ -0,0 +1,6 @@
+class AssetsService {
+  static String imagePath = "/imgs";
+  static String person = "$imagePath/person.png";
+  static String bot = "$imagePath/bot_img.png";
+  static String gptLogo = "$imagePath/gptlogo_png.png";
+}
