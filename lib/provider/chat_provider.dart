70f4e8a043f386dc3ecc44ee7b55474aeb1f7af8
krishpranav <krisna.pranav@gmail.com>
2023-04-22 13:28:09 +0530

    provider: chat_provider

---
+++ lib/provider/chat_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:chatgpt/models/model.dart';
+import 'package:chatgpt/services/api_service.dart';
+
+import '../models/chat_models.dart';
+
+class ChatProvider with ChangeNotifier {
+  List<ChatModel> chatList = [];
+  List<ChatModel> get getChatList {
+    return chatList;
+  }
+
+  void addUserMessage({required String msg}) {
+    chatList.add(ChatModel(msg: msg, chatIndex: 0));
+    notifyListeners();
+  }
+
+  Future<void> sendMessageAndGetAnswers(
+      {required String msg, required String chosenModelId}) async {
+    if (chosenModelId.toLowerCase().startsWith("gpt")) {
+      chatList.addAll(await ApiService.sendMessageGPT(
+        message: msg,
+        modelId: chosenModelId,
+      ));
+    } else {
+      chatList.addAll(await ApiService.sendMessage(
+        message: msg,
+        modelId: chosenModelId,
+      ));
+    }
+    notifyListeners();
+  }
+}
